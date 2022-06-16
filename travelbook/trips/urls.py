bdf3b449f843e523b0d288ab2adc082f73893f75
Saul1US <leaderis13@gmail.com>
2022-06-16 06:52:34 -0500

    views fixed

--- travelbook/trips/urls.py
+++ travelbook/trips/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('trip_list/', views.trip_list_view, name='trip_list'),
+    # path('trip_list/', views.trip_list_view, name='trip_list'),
+    path('trip/<int:id>', views.trip, name='trip'),
+    path('trip_list/', views.TripListView.as_view(), name='trip_list'),
+    path('place_list/', views.PlaceListView.as_view(), name='place_list'),
     path('place_details/<int:id>/', views.place_details_view, name='place_details'),
 ]
--- travelbook/trips/views.py
+++ travelbook/trips/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Trip, Place, PlaceImage
 
 
 def home(request):
     return render(request, 'trips/home.html')
 
-def trip_list_view(request):
-    places = Place.objects.all()
-    return render(request, 'trips/trip_list.html', {'places':places})
+def trip(request, id):
+    trip = get_object_or_404(Trip, id=id)
+    return render(request, 'trips/trip.html', {'trip':trip})
+
+# def trip_list_view(request):
+#     places = Place.objects.all()
+#     return render(request, 'trips/trip_list.html', {'places':places})
  
 def place_details_view(request, id):
     place = get_object_or_404(Place, id=id)
     photos = PlaceImage.objects.filter(place=place)
-    return render(request, 'trips/place_details.html', {
-        'place':place,
-        'photos':photos, 
-    })
+    return render(request, 'trips/place_details.html', {'place':place, 'photos':photos, })
+
+
+class TripListView(generic.ListView):
+    model = Trip
+    context_object_name = 'trips'
+    template_name = 'trips/trip_list.html'
+
+
+class PlaceListView(generic.ListView):
+    model = Place
+    context_object_name = 'places'
+    template_name = 'trips/place_list.html'
+
+
+class PlaceDetailView(generic.DetailView):
+    model = Place
+    template_name = 'trips/place_details.html'
